ec4e8150848e224dd4354a58f37b4488b3ec478d
Ben Rosen <benrosen78@gmail.com>
2017-07-27 16:33:46 -0500

    Initial Commit

---
+++ DannyBenWorkout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DannyBenWorkout
+//
+//  Created by Ben Rosen on 7/27/17.
+//  Copyright © 2017 Ben Rosen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DannyBenWorkout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DannyBenWorkout
+//
+//  Created by Ben Rosen on 7/27/17.
+//  Copyright © 2017 Ben Rosen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
