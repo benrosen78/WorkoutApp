2562d4ffb2edcd46255442694a5152908bd20a27
Ben Rosen <ben@benrosen.me>
2017-10-29 11:57:46 -0500

    [stopwatch] add cancel and pause buttons

--- Workout/Stopwatch/DBWStopwatchActiveViewController.m
+++ Workout/Stopwatch/DBWStopwatchActiveViewController.m
@@ -13,6 +13,8 @@
 
 @property (strong, nonatomic) UILabel *timeLabel;
 
+@property (strong, nonatomic) UIButton *cancelButton, *pauseButton;
+
 @end
 
 @implementation DBWStopwatchActiveViewController
@@ -34,6 +36,37 @@
     [_timeLabel.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:80].active = YES;
 
     self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    
+    _pauseButton = [[UIButton alloc] init];
+    _pauseButton.backgroundColor = [UIColor appTintColor];
+    _pauseButton.layer.masksToBounds = YES;
+    _pauseButton.layer.cornerRadius = 50;
+    [_pauseButton setTitle:@"Pause" forState:UIControlStateNormal];
+    [_pauseButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    _pauseButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:_pauseButton];
+    
+    [_pauseButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+    [_pauseButton.heightAnchor constraintEqualToConstant:100].active = YES;
+    [_pauseButton.widthAnchor constraintEqualToConstant:100].active = YES;
+    [_pauseButton.topAnchor constraintEqualToAnchor:_timeLabel.bottomAnchor constant:60].active = YES;
+
+    
+    _cancelButton = [[UIButton alloc] init];
+    _cancelButton.backgroundColor = [UIColor appTintColor];
+    _cancelButton.layer.masksToBounds = YES;
+    _cancelButton.layer.cornerRadius = 50;
+    [_cancelButton setTitle:@"Cancel" forState:UIControlStateNormal];
+    [_cancelButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    _cancelButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:_cancelButton];
+    
+    [_cancelButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+    [_cancelButton.heightAnchor constraintEqualToConstant:100].active = YES;
+    [_cancelButton.widthAnchor constraintEqualToConstant:100].active = YES;
+    [_cancelButton.topAnchor constraintEqualToAnchor:_pauseButton.bottomAnchor constant:40].active = YES;
+    
+    
     
     
     
