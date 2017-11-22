7ae39bdd647b7e66cc9be04a94a4d8ac38b8c7ee
Ben Rosen <ben@benrosen.me>
2017-11-22 11:51:28 -0600

    [exercise-database] segmented control works now for filtering, table updates correctly

--- Workout/Exercise Database/DBWExerciseDatabaseTableViewController.m
+++ Workout/Exercise Database/DBWExerciseDatabaseTableViewController.m
@@ -20,6 +20,8 @@ static NSString *const kCellIdentifier = @"exercise-placeholder-cell";
 
 @property (strong, nonatomic) DBWExerciseDatabase *exerciseDatabasePlaceholders;
 
+@property (strong, nonatomic) RLMResults *filteredExercisePlaceholders;
+
 @property (strong, nonatomic) UIVisualEffectView *blurredBackgroundView;
 @end
 
@@ -29,31 +31,32 @@ static NSString *const kCellIdentifier = @"exercise-placeholder-cell";
     [super viewDidLoad];
     
     _exerciseDatabasePlaceholders = [[DBWDatabaseManager sharedDatabaseManager] allExercisePlaceholders];
-    
+    _filteredExercisePlaceholders = [_exerciseDatabasePlaceholders.list objectsWithPredicate:[NSPredicate predicateWithFormat:@"type == 0"]];
+
     self.title = @"Exercise Database";
     
     self.navigationController.navigationBar.prefersLargeTitles = NO;
     
-    self.tableView.tableHeaderView = [[UISegmentedControl alloc] initWithItems:@[@"Chest", @"Back", @"Arms", @"Shoulders", @"Legs", @"Core"]];
-    
-    
+    UISegmentedControl *segmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"Chest", @"Back", @"Shoulders", @"Core", @"Arms", @"Legs"]];
+    [segmentedControl setSelectedSegmentIndex:0];
+    self.tableView.tableHeaderView = segmentedControl;
+    [segmentedControl addTarget:self action:@selector(filterChanged:) forControlEvents:UIControlEventValueChanged];
+
     UISearchController *searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
     self.navigationItem.searchController = searchController;
     self.navigationItem.hidesSearchBarWhenScrolling = NO;
     
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addExercise:)];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelTapped)];
-    
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)cancelTapped {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)filterChanged:(UISegmentedControl *)control {
+    _filteredExercisePlaceholders = [_exerciseDatabasePlaceholders.list objectsWithPredicate:[NSPredicate predicateWithFormat:@"type == %lu", control.selectedSegmentIndex]];
+    [self.tableView reloadData];
 }
 
 - (void)addExercise:(UIBarButtonItem *)sender {
@@ -91,7 +94,7 @@ static NSString *const kCellIdentifier = @"exercise-placeholder-cell";
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [_exerciseDatabasePlaceholders.list count];
+    return [_filteredExercisePlaceholders count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -99,7 +102,7 @@ static NSString *const kCellIdentifier = @"exercise-placeholder-cell";
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kCellIdentifier];
     }
-    DBWExercisePlaceholder *placeholder = _exerciseDatabasePlaceholders.list[indexPath.row];
+    DBWExercisePlaceholder *placeholder = _filteredExercisePlaceholders[indexPath.row];
     cell.textLabel.text = placeholder.name;
     
     return cell;
@@ -108,7 +111,7 @@ static NSString *const kCellIdentifier = @"exercise-placeholder-cell";
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    DBWExercisePlaceholder *placeholder = _exerciseDatabasePlaceholders.list[indexPath.row];
+    DBWExercisePlaceholder *placeholder = _filteredExercisePlaceholders[indexPath.row];
     
     
     DBWExerciseDatabaseConfirmationViewController *confirmationViewController = [[DBWExerciseDatabaseConfirmationViewController alloc] init];
@@ -125,20 +128,23 @@ static NSString *const kCellIdentifier = @"exercise-placeholder-cell";
 }
 
 - (void)creationViewController:(DBWExercisePlaceholderCreationViewController *)creationViewController finishedWithMuscleGroup:(DBWExercisePlaceholderType)group andExerciseName:(NSString *)name {
-    
-    DBWExercisePlaceholder *exercisePlaceholder = [[DBWExercisePlaceholder alloc] init];
-    exercisePlaceholder.type = group;
-    exercisePlaceholder.name = name;
-    [[DBWDatabaseManager sharedDatabaseManager] saveNewExercisePlaceholder:exercisePlaceholder];
-    [self.tableView reloadData];
-    
+    self.tableView.userInteractionEnabled = YES;
+
     DBWExercisePlaceholderCreationViewController *__block localCreationViewController = creationViewController;
     UIViewPropertyAnimator *creationViewAnimator = [[UIViewPropertyAnimator alloc] initWithDuration:0.4 dampingRatio:0.8 animations:^{
         localCreationViewController.view.frame = CGRectMake(0, [[UIScreen mainScreen] bounds].size.height, [[UIScreen mainScreen] bounds].size.width, 460);
     }];
     [creationViewAnimator addCompletion:^(UIViewAnimatingPosition finalPosition) {
         [localCreationViewController.view removeFromSuperview];
+        [localCreationViewController willMoveToParentViewController:nil];
         localCreationViewController = nil;
+        
+        DBWExercisePlaceholder *exercisePlaceholder = [[DBWExercisePlaceholder alloc] init];
+        exercisePlaceholder.type = group;
+        exercisePlaceholder.name = name;
+        [[DBWDatabaseManager sharedDatabaseManager] saveNewExercisePlaceholder:exercisePlaceholder];
+        [self.tableView reloadData];
+        
     }];
     [creationViewAnimator startAnimation];
     
